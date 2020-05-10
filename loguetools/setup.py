c20513a90afc9b1c19a91f38703c939e9f8bebdd
gary <gary.ruben@gmail.com>
2020-05-10 20:55:31 +1000

    Set version for release

--- loguetools/setup.py
+++ loguetools/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    version="0.0.1",
+    version="0.1.0",
     author="Gary Ruben",
     author_email="gary.ruben@gmail.com",
     description="Korg minilogue family patch manipulation tools",
