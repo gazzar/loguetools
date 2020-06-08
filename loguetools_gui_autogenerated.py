e0b885d45e7aa3306d6cbd54110d54774c053f13
gary <gary.ruben@gmail.com>
2020-06-08 15:57:54 +1000

    Tweak gui

--- loguetools_gui_autogenerated.py
+++ loguetools_gui_autogenerated.py
@@ -21,28 +21,28 @@ class MainFrame ( wx.Frame ):
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
-        bSizer = wx.BoxSizer( wx.VERTICAL )
+        MainFrameSizer = wx.BoxSizer( wx.VERTICAL )
 
-        gSizer1 = wx.GridSizer( 0, 2, 0, 0 )
+        toolbarSizer = wx.GridSizer( 0, 2, 0, 0 )
 
-        self.m_panel3 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        bSizer5 = wx.BoxSizer( wx.VERTICAL )
+        self.toolbarPanel = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        toolbarPanelSizer = wx.BoxSizer( wx.VERTICAL )
 
-        self.toolbar = wx.ToolBar( self.m_panel3, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_HORIZONTAL )
+        self.toolbar = wx.ToolBar( self.toolbarPanel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_HORIZONTAL )
         self.toolbar.Realize()
 
-        bSizer5.Add( self.toolbar, 0, wx.EXPAND, 0 )
+        toolbarPanelSizer.Add( self.toolbar, 0, wx.ALL|wx.EXPAND, 0 )
 
 
-        self.m_panel3.SetSizer( bSizer5 )
-        self.m_panel3.Layout()
-        bSizer5.Fit( self.m_panel3 )
-        gSizer1.Add( self.m_panel3, 1, wx.EXPAND, 0 )
+        self.toolbarPanel.SetSizer( toolbarPanelSizer )
+        self.toolbarPanel.Layout()
+        toolbarPanelSizer.Fit( self.toolbarPanel )
+        toolbarSizer.Add( self.toolbarPanel, 1, wx.EXPAND, 0 )
 
-        self.m_panel4 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        bSizer4 = wx.BoxSizer( wx.VERTICAL )
+        self.checkboxPanel = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        checkboxPanelSizer = wx.BoxSizer( wx.VERTICAL )
 
-        self.m_toolBar_options = wx.ToolBar( self.m_panel4, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_HORIZONTAL )
+        self.m_toolBar_options = wx.ToolBar( self.checkboxPanel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_HORIZONTAL )
         self.m_checkBox_id = wx.CheckBox( self.m_toolBar_options, wx.ID_ANY, u"id", wx.DefaultPosition, wx.DefaultSize, 0 )
         self.m_checkBox_id.SetToolTip( u"Prepend id to filename" )
 
@@ -61,56 +61,56 @@ class MainFrame ( wx.Frame ):
         self.m_toolBar_options.AddControl( self.m_checkBox_inits )
         self.m_toolBar_options.Realize()
 
-        bSizer4.Add( self.m_toolBar_options, 0, wx.EXPAND, 0 )
+        checkboxPanelSizer.Add( self.m_toolBar_options, 0, wx.ALL|wx.EXPAND, 0 )
 
 
-        self.m_panel4.SetSizer( bSizer4 )
-        self.m_panel4.Layout()
-        bSizer4.Fit( self.m_panel4 )
-        gSizer1.Add( self.m_panel4, 1, wx.EXPAND, 0 )
+        self.checkboxPanel.SetSizer( checkboxPanelSizer )
+        self.checkboxPanel.Layout()
+        checkboxPanelSizer.Fit( self.checkboxPanel )
+        toolbarSizer.Add( self.checkboxPanel, 1, wx.EXPAND, 0 )
 
 
-        bSizer.Add( gSizer1, 0, wx.EXPAND, 5 )
+        MainFrameSizer.Add( toolbarSizer, 0, wx.EXPAND, 0 )
 
-        fgSizer1 = wx.FlexGridSizer( 1, 2, 0, 0 )
-        fgSizer1.AddGrowableCol( 0 )
-        fgSizer1.AddGrowableCol( 1 )
-        fgSizer1.AddGrowableRow( 0 )
-        fgSizer1.SetFlexibleDirection( wx.BOTH )
-        fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+        displaySizer = wx.FlexGridSizer( 1, 2, 0, 0 )
+        displaySizer.AddGrowableCol( 0 )
+        displaySizer.AddGrowableCol( 1 )
+        displaySizer.AddGrowableRow( 0 )
+        displaySizer.SetFlexibleDirection( wx.BOTH )
+        displaySizer.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
 
-        self.m_splitter2 = wx.SplitterWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.SP_3D|wx.SP_LIVE_UPDATE )
-        self.m_splitter2.Bind( wx.EVT_IDLE, self.m_splitter2OnIdle )
-        self.m_splitter2.SetMinimumPaneSize( 40 )
+        self.displaySplitter = wx.SplitterWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.SP_3D|wx.SP_LIVE_UPDATE )
+        self.displaySplitter.Bind( wx.EVT_IDLE, self.displaySplitterOnIdle )
+        self.displaySplitter.SetMinimumPaneSize( 40 )
 
-        self.m_panel1 = wx.Panel( self.m_splitter2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        bSizer2 = wx.BoxSizer( wx.VERTICAL )
+        self.patchListPanel = wx.Panel( self.displaySplitter, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        patchListPanelSizer = wx.BoxSizer( wx.VERTICAL )
 
-        self.listCtrl = wx.ListCtrl( self.m_panel1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LC_REPORT )
-        bSizer2.Add( self.listCtrl, 1, wx.EXPAND, 5 )
-
-
-        self.m_panel1.SetSizer( bSizer2 )
-        self.m_panel1.Layout()
-        bSizer2.Fit( self.m_panel1 )
-        self.m_panel2 = wx.Panel( self.m_splitter2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        bSizer3 = wx.BoxSizer( wx.VERTICAL )
-
-        self.m_textCtrlOut = wx.TextCtrl( self.m_panel2, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.HSCROLL|wx.TE_MULTILINE|wx.TE_READONLY )
-        bSizer3.Add( self.m_textCtrlOut, 2, wx.EXPAND, 5 )
+        self.listCtrl = wx.ListCtrl( self.patchListPanel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LC_REPORT )
+        patchListPanelSizer.Add( self.listCtrl, 1, wx.EXPAND, 5 )
 
 
-        self.m_panel2.SetSizer( bSizer3 )
-        self.m_panel2.Layout()
-        bSizer3.Fit( self.m_panel2 )
-        self.m_splitter2.SplitVertically( self.m_panel1, self.m_panel2, 510 )
-        fgSizer1.Add( self.m_splitter2, 1, wx.EXPAND, 5 )
+        self.patchListPanel.SetSizer( patchListPanelSizer )
+        self.patchListPanel.Layout()
+        patchListPanelSizer.Fit( self.patchListPanel )
+        self.stdoutPanel = wx.Panel( self.displaySplitter, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        stdoutPanelSizer = wx.BoxSizer( wx.VERTICAL )
+
+        self.m_textCtrlOut = wx.TextCtrl( self.stdoutPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.HSCROLL|wx.TE_MULTILINE|wx.TE_READONLY )
+        stdoutPanelSizer.Add( self.m_textCtrlOut, 2, wx.EXPAND, 5 )
 
 
-        bSizer.Add( fgSizer1, 1, wx.EXPAND, 5 )
+        self.stdoutPanel.SetSizer( stdoutPanelSizer )
+        self.stdoutPanel.Layout()
+        stdoutPanelSizer.Fit( self.stdoutPanel )
+        self.displaySplitter.SplitVertically( self.patchListPanel, self.stdoutPanel, 510 )
+        displaySizer.Add( self.displaySplitter, 1, wx.EXPAND, 5 )
 
 
-        self.SetSizer( bSizer )
+        MainFrameSizer.Add( displaySizer, 1, wx.EXPAND, 0 )
+
+
+        self.SetSizer( MainFrameSizer )
         self.Layout()
         self.statusBar = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
         self.m_menubar1 = wx.MenuBar( 0 )
@@ -154,8 +154,8 @@ class MainFrame ( wx.Frame ):
     def OnAbout( self, event ):
         event.Skip()
 
-    def m_splitter2OnIdle( self, event ):
-    	self.m_splitter2.SetSashPosition( 510 )
-    	self.m_splitter2.Unbind( wx.EVT_IDLE )
+    def displaySplitterOnIdle( self, event ):
+    	self.displaySplitter.SetSashPosition( 510 )
+    	self.displaySplitter.Unbind( wx.EVT_IDLE )
 
 
