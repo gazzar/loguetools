c717b4f01de14fb62d567dd189495b8d035e69b0
gary <gary.ruben@gmail.com>
2023-04-07 14:21:40 +1000

    bumped Python version dependency

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.10',
 )
